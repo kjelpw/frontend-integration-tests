1383c9d0ce78bac6939cd92e7a6da72e0cc2df92
kjepette <kjel.pettenger-willey@du.edu>
2020-05-26 10:21:12 -0700

    another basic test

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 describe('Array', function() {
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
-      assert.equal([1, 2, 3].indexOf(4), -1);
+      assert.equal([1, 2, 3].indexOf(0), 1);
     });
   });
 });
